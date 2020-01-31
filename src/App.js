11f27963e156e3a5f21e269a33e6c31c8a545d13
IT2 <it2@AWT.local>
2020-01-31 14:09:02 -0600

    wip

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import dummydata from "./dummy-data.js"
+import PostsPage from "./components/PostsContainer/PostsPage.js"
+import SearchBar from "./components/SearchBar/SearchBarContainer.js"
+import Post from "./components/PostsContainer/Post";
+
 // import the PostsPage and SearchBar and add them to the App
 // import dummydata
 
 const App = () => {
   // set up state for the dummy data and pass to your PostsPage
+  let [post, setPost] = useState(dummydata)
+
   return (
     <div className="App">
+      <SearchBar/>
+      <PostsPage post={post}/>
+      
       {/* Add components here  and pass props where appropriate */}
     </div>
   );
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,11 +3,21 @@ import React from "react";
 import Post from "./Post";
 import "./Posts.css";
 
+
+
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = props => {
+
+ 
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here */}
+      
+      {props.post.map(item => (
+        <Post
+          key = {item.imageUrl}
+          post = {item}
+        />
+      ))} 
     </div>
   );
 };
